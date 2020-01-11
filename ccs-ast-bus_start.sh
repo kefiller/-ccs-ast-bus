cc75452b509c9b5ed8698ae0fd586586d12db057
Evgeny Erofeev <erofeev@regenergy.ru>
2020-01-11 23:20:47 +0300

    node run vars

--- ccs-ast-bus_start.sh
+++ ccs-ast-bus_start.sh
@@ -8,8 +8,10 @@ docker run -t --rm  -w /app \
     --name=ccs-ast-bus \
     --network=host \
     -v $SCRIPT_ORIGDIR:/app \
+    -e NODE_DISABLE_COLORS=1 \
     -e PBX_NAME=`hostname -A` \
     -e AMI_HOST='localhost' \
     -e AMI_USER=api \
     -e AMI_PASSWORD='Fgbitxrf18' \
+    -e LOG_LEVEL="info" \
     node:latest node index.js $@
